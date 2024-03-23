c87a7d4d943ae688684e0dac33b586f051d38f7f
pbamin <pbamin07@gmail.com>
2024-03-23 17:58:27 +0800

    first commit

---
+++ StoryMethods.py
@@ -0,0 +1,62 @@
+class StoryMethods:
+
+    def story_ai(msg,client):
+        story_response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{
+                "role": "system", 
+                "content": """You are a bestseller story writer.
+                            You'll take user's prompt and generate a 100 words shortstory for children age 8-10"""
+                },
+                {
+                "role": "user", 
+                "content": f'{msg}'
+                }
+            ],
+            max_tokens= 400,
+            temperature= 1.3,
+        )
+
+        story = story_response.choices[0].message.content
+        #print(story)
+
+        return story
+
+    def cover_ai(msg,client):
+        response_cover = client.images.generate(
+            model="dall-e-3",
+            prompt=f"{msg} in ghibli style",
+            size="1024x1024",
+            quality="standard",
+            n=1,
+        )
+
+        image_url = response_cover.data[0].url
+        #display(Image(url=image_url))
+
+        return image_url
+    
+    def design_ai(msg,client):
+        design_response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{
+
+                "role": "system",
+                "content": """"Based on the story given,
+                            You will design a detailed image prompt for the cover image of this story.
+                            The image prompt should include theme of the story with relevant color suitable for children book.
+                            The output should be within 100 characters"""
+                },
+                {
+                "role":"user",
+                "content":f'{msg}'
+                }
+            ],
+            max_tokens= 100,
+            temperature= 1.3
+        )
+        design_prompt = design_response.choices[0].message.content
+        #print(design_prompt)
+
+        return design_prompt
+    
---
+++ storybook_ai.py
@@ -0,0 +1,38 @@
+# 004 AI Children Storybook Generator
+## 1.0 Story Generator
+import os
+from openai import OpenAI
+from IPython.display import Image
+import streamlit as st
+from StoryMethods import StoryMethods as sm
+
+client=OpenAI(
+    api_key = st.secrets["OPEN_API_KEY"]
+    )
+
+def main():
+
+    #Streamlit app layout
+    st.title('Children Story Generator')
+
+    #User input for story generator
+    user_prompt = st.text_area('Enter a prompt for the story')
+    if st.button('Generate Story'):
+
+        story = sm.story_ai(user_prompt,client)
+        design = sm.design_ai(story,client)
+        image_url = sm.cover_ai(design,client)
+
+        st.image(image_url, caption= 'Generated Cover Image', use_column_width=True)
+ 
+        st.write('Generated Image Prompt: ')
+        st.write(design)
+
+        st.divider()
+
+        st.write('Generated Story: ')
+        st.write(story)
+
+
+if __name__=="__main__":
+    main()
